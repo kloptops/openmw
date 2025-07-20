592fa1dd9cb05ec76a8bf8452279dd4fa292a73d
Jacob Smith <kloptops@gmail.com>
2025-07-20 11:07:48 +0800

    Fixed cursor on ROCKNIX after touching screen, added better debugging for shader code.

--- components/sdlutil/sdlinputwrapper.cpp
+++ components/sdlutil/sdlinputwrapper.cpp
@@ -348,6 +348,7 @@ namespace SDLUtil
     void InputWrapper::updateMouseSettings()
     {
         mGrabPointer = mWantGrab && mMouseInWindow && mWindowHasFocus;
+        mGrabPointer = true;
         SDL_SetWindowGrab(mSDLWindow, mGrabPointer && mAllowGrab ? SDL_TRUE : SDL_FALSE);
 
         SDL_ShowCursor(mWantMouseVisible || !mWindowHasFocus);
